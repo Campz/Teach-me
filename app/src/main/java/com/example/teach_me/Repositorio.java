465fbc02e67b50617bc474733e57cfd135e9e7d9
RuanKenned <61585983+RuanKenned@users.noreply.github.com>
2021-02-12 08:21:45 -0300

    GetAll de todas as models

--- app/src/main/java/com/example/teach_me/Repositorio.java
+++ app/src/main/java/com/example/teach_me/Repositorio.java
@@ -1,16 +1,13 @@
 package com.example.teach_me;
 
-import android.util.Log;
-import android.widget.Toast;
-
-import com.example.teach_me.api.UsuarioData;
 import com.example.teach_me.models.Anuncio;
+import com.example.teach_me.models.Aula;
 import com.example.teach_me.models.Disciplina;
+import com.example.teach_me.models.Instituicao;
 import com.example.teach_me.models.Tipo;
 import com.example.teach_me.models.Usuario;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -20,10 +17,18 @@ public class Repositorio {
     private static Repositorio repositorio;
     private ArrayList<Usuario> usuarios;
     private ArrayList<Anuncio> anuncios;
+    private ArrayList<Aula> aulas;
+    private ArrayList<Disciplina> disciplinas;
+    private ArrayList<Tipo> tipos;
+    private ArrayList<Instituicao> instituicoes;
 
     private Repositorio(){
         this.usuarios = new ArrayList<>();
         this.anuncios = new ArrayList<>();
+        this.aulas = new ArrayList<>();
+        this.disciplinas = new ArrayList<>();
+        this.tipos = new ArrayList<>();
+        this.instituicoes = new ArrayList<>();
     }
 
     public static Repositorio getInstance(){
@@ -35,37 +40,129 @@ public class Repositorio {
     }
 
     public ArrayList<Usuario> getUsuarios(){
-        Call<List<Usuario>> call = new RetrofitClientInstance().getUsuarioService().buscarListUsuario("0");
+        /*Call<List<Usuario>> call = new RetrofitClientInstance().getUsuarioService().buscarListUsuario("1");
         call.enqueue(new Callback<List<Usuario>>() {
             @Override
             public void onResponse(Call<List<Usuario>> call, Response<List<Usuario>> response) {
                if(response.isSuccessful()){
-                   usuarios = new ArrayList<>(response.body());
-                   Log.w("OPA", usuarios.get(0).getData().getEmail());
+                   for (int i = 0; i < 5; i++) {
+                       usuarios.add(response.body().get(i));
+                   }
+                   System.out.println(usuarios.get(0).getData().toString());
+                   //usuarios = new ArrayList<>(response.body());
                }
             }
 
             @Override
             public void onFailure(Call<List<Usuario>> call, Throwable t) {
             }
-        });
+        });*/
+        for (int i = 1; i <= 5; i++){
+            String aux = Integer.toString(i);
+            Call<Usuario> call = new RetrofitClientInstance().getUsuarioService().buscarUsuario(aux);
+            call.enqueue(new Callback<Usuario>() {
+                @Override
+                public void onResponse(Call<Usuario> call, Response<Usuario> response) {
+                        Usuario usu = response.body();
+                        usuarios.add(usu);
+                        System.out.println(usuarios.get(0).getData().toString());
+                }
+                @Override
+                public void onFailure(Call<Usuario> call, Throwable t) {
+                }
+            });
+        }
+
         return usuarios;
     }
 
     public ArrayList<Anuncio> getAnuncios(){
-        Call<List<Anuncio>> call = new RetrofitClientInstance().getAnuncioService().buscarListAnuncio("1");
-        call.enqueue(new Callback<List<Anuncio>>() {
-            @Override
-            public void onResponse(Call<List<Anuncio>> call, Response<List<Anuncio>> response) {
-                if(response.isSuccessful()){
-                    anuncios = new ArrayList<>(response.body());
+        for (int i = 9; i <= 12; i++){
+            String aux = Integer.toString(i);
+            Call<Anuncio> call = new RetrofitClientInstance().getAnuncioService().buscarAnuncio(aux);
+            call.enqueue(new Callback<Anuncio>() {
+                @Override
+                public void onResponse(Call<Anuncio> call, Response<Anuncio> response) {
+                    Anuncio a = response.body();
+                    anuncios.add(a);
                 }
-            }
-
-            @Override
-            public void onFailure(Call<List<Anuncio>> call, Throwable t) {
-            }
-        });
+                @Override
+                public void onFailure(Call<Anuncio> call, Throwable t) {
+                }
+            });
+        }
         return anuncios;
+    }
+
+    public ArrayList<Aula> getAulas(){
+        for (int i = 7; i <= 11; i++){
+            String aux = Integer.toString(i);
+            Call<Aula> call = new RetrofitClientInstance().getAulaService().buscarAula(aux);
+            call.enqueue(new Callback<Aula>() {
+                @Override
+                public void onResponse(Call<Aula> call, Response<Aula> response) {
+                    Aula a = response.body();
+                    aulas.add(a);
+                }
+                @Override
+                public void onFailure(Call<Aula> call, Throwable t) {
+                }
+            });
+        }
+        return aulas;
+    }
+
+    public ArrayList<Disciplina> getDisciplinas(){
+        for (int i = 1; i <= 6; i++){
+            String aux = Integer.toString(i);
+            Call<Disciplina> call = new RetrofitClientInstance().getDisciplinaService().buscarDisciplina(aux);
+            call.enqueue(new Callback<Disciplina>() {
+                @Override
+                public void onResponse(Call<Disciplina> call, Response<Disciplina> response) {
+                    Disciplina d = response.body();
+                    disciplinas.add(d);
+                }
+                @Override
+                public void onFailure(Call<Disciplina> call, Throwable t) {
+                }
+            });
+        }
+        return disciplinas;
+    }
+
+    public ArrayList<Instituicao> getInstituicoes(){
+        for (int i = 1; i <= 2; i++){
+            String aux = Integer.toString(i);
+            Call<Instituicao> call = new RetrofitClientInstance().getInstituicaoService().buscarInstituicao(aux);
+            call.enqueue(new Callback<Instituicao>() {
+                @Override
+                public void onResponse(Call<Instituicao> call, Response<Instituicao> response) {
+                    Instituicao inst = response.body();
+                    instituicoes.add(inst);
+                }
+                @Override
+                public void onFailure(Call<Instituicao> call, Throwable t) {
+                }
+            });
+        }
+        return instituicoes;
+    }
+
+    public ArrayList<Tipo> getTipos(){
+        for (int i = 1; i <= 3; i++){
+            String aux = Integer.toString(i);
+            Call<Tipo> call = new RetrofitClientInstance().getTipoService().buscarTipo(aux);
+            call.enqueue(new Callback<Tipo>() {
+                @Override
+                public void onResponse(Call<Tipo> call, Response<Tipo> response) {
+                    Tipo t = response.body();
+                    tipos.add(t);
+                }
+                @Override
+                public void onFailure(Call<Tipo> call, Throwable t) {
+                }
+            });
+        }
+        return tipos;
     }
 }
