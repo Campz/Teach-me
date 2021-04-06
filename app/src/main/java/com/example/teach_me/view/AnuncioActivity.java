807b3a07e83636d9a1b0af908a0155b30e134d08
Rafael Campos <rafacampz00@gmail.com>
2021-04-06 20:24:53 -0300

    Anuncio Activity + Layout

---
+++ app/src/main/java/com/example/teach_me/view/AnuncioActivity.java
@@ -0,0 +1,24 @@
+package com.example.teach_me.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.teach_me.R;
+
+public class AnuncioActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anuncio);
+    }
+
+    private void initComponents(){
+
+    }
+
+    private void buttonsEvents(){
+
+    }
+}
